7c9015fe743b4679851e65a890730ac98e57b0c1
Taylor Donlan <taylordonlan@gmail.com>
2017-11-14 19:37:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,14 @@
 def fizzbuzz(int)
-  if (int % 3 == 0 && int % 5 == 0)
-    "FizzBuzz"
+  if (int % 3 == 0)
+      if (int % 5 == 0)
+        "FizzBuzz"
+      else
+        "Fizz"
+      end
 
-elseif int % 3 == 0
-"Fizz"
-
-elseif int % 5 == 0
+  elseif int % 5 == 0
 "Buzz"
+
 
 else
   nil
